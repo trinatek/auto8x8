518a4308c248b86f546b672fba9d795e7c08762f
Napoleon Vang <trinatek@gmail.com>
2023-11-07 11:06:20 -0800

    Testing thrown error in code.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@
   
   const contactName = process.env.CONTACT_NAME;
   const contactPhoneNumber = process.env.CONTACT_PHONE_NUMBER;
+
+  // Testing failed job
+  throw new Error("🔴 Test error has been thrown...");
   
   console.log(`    • Contact name (process.env.CONTACT_NAME): '${contactName}'`);
   console.log(`    • Contact phone number (process.env.CONTACT_PHONE_NUMBER): '${contactPhoneNumber}'`);
